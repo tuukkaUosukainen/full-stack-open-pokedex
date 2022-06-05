aeab1f56bd90214feb6e1680293ce473cfd63f65
Kapteeni Tuukka <kapteeni@example.com>
2022-06-05 18:28:16 +0300

    fix eslint problems in cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,10 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
+// eslint-disable-next-line no-undef
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
+    setupNodeEvents() {
       // implement node event listeners here
     },
   },
-});
+})
